6eea224ff4dfc24bf41d52c475faca29fe73c2a0
Vivekksharma21 <21cse677.vivekkumar@giet.edu>
2024-07-07 23:09:45 +0530

    Routes add

---
+++ Backend/Routes/auth.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { signup , login , logout } from "../controllers/auth.js";
+ 
+const router = express.Router();
+
+router.get("/signup" , signup);
+
+router.get("/login" , login);
+
+router.get("/logout" , logout);
+
+
+
+export default router;
+
+
+
