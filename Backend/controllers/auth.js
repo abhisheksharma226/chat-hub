77a438923bb54006c84a567125da01614b471af6
Vivekksharma21 <21cse677.vivekkumar@giet.edu>
2024-07-07 23:08:21 +0530

    controllers adds

---
+++ Backend/controllers/auth.js
@@ -0,0 +1,11 @@
+export const signup = (req , res) => {
+    res.send("signup");
+}
+
+export const login = (req , res) => {
+    res.send("login");
+}
+
+export const logout = (req , res) => {
+    res.send("logout");
+}
