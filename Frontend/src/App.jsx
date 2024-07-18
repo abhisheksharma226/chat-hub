bbcc58e4dc053ad58feb50490f6dd0869c399037
abhisheksharma226 <avisheksharma2004@gmail.com>
2024-07-18 11:44:29 +0530

    Login Sigup Done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css';
 import Login from "./pages/login/login";
+import Signup from './pages/signup/singup';
+import Home from './pages/home/home'
 
 function App() {
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      <Login />
+      <Signup />
     </div>
   )
 }
 
 export default App;
+ 
